0453c2da05aeb191ba9696b217f02379130e4e39
sumeyrakarsavran <bir-kar-12@hotmail.com>
2021-02-13 23:39:49 +0300

    something was added

---
+++ MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " Kişisi Eklendi");
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " Kişisi silindi");
+        }
+
+        public void MusteriListele(Musteri[] musteri)
+        {
+            foreach (var item in musteri)
+            {
+                Console.WriteLine(item.Id + " " + item.Ad + " " + item.Soyad + " ");
+            }
+        }
+    }
+}
