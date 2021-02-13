13f6226f4771eed46236c2c41b5ce8688d92c837
sumeyrakarsavran <bir-kar-12@hotmail.com>
2021-02-13 23:26:55 +0300

    ClassMetotDemo is added.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Sümeyra";
+            musteri1.Soyad = "Karsavran";
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Eren";
+            musteri2.Soyad = "Karsavran";
+            musteri2.Id = 2;
+
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Şevval";
+            musteri3.Soyad = "Aydın";
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriEkle(musteri3);
+
+            musteriManager.MusteriSil(musteri1);
+
+            Console.WriteLine("Müşteri Listesi");
+            musteriManager.MusteriListele(musteriler);
+        }
+    }
+}
